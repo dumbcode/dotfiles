a72030a5696080db7006fecf91182e5ba3ffe49d
dumbcode <dumbcode@gmail.com>
2025-05-20 20:13:42 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ wget https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf
 mkdir -p ~/cli/sf
 tar xJf sf-linux-x64.tar.xz -C ~/cli/sf --strip-components 1
 export PATH=~/cli/sf/bin:$PATH
+echo 'export PATH=~/cli/sf/bin:$PATH' >> ~/.bashrc
